084b1899e4d1599f6c853f48f962f3a9d16409ea
tashfeenrao <raotashfeen25@gmail.com>
2020-10-14 22:56:04 +0500

    use effect use for side effect in component

--- src/components/items/SingleItem.js
+++ src/components/items/SingleItem.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../css/singleItem.css";
 
 const SingleItem = () => {
+  const [count, setCount] = useState(0);
+  const [item, setItem] = useState(0);
+  useEffect(() => {
+    if (count === 1) {
+      setItem(1);
+    }
+  }, [count]);
+
   return (
     <div>
-      <h1>I'm single item</h1>
+      <Link to="/">Back</Link>
+      <div className="singleItem__contianer">
+        <h1>single item</h1>
+        <h1>{item}</h1>
+        <button onClick={() => setCount(count + 1)}>add item</button>
+      </div>
     </div>
   );
-}
+};
 
 export default SingleItem;
